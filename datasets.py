f896f2b31a6bd750f37d697d36333df8cf150b1c
Aditya Parikh <85225049+ADE-17@users.noreply.github.com>
2023-10-28 23:35:30 +0200

    Added lightning trainer

--- datasets.py
+++ datasets.py
@@ -10,7 +10,11 @@ class CustomCancerDataset(Dataset):
     def __init__(self, metadata_df, image_folder, transform=None):
         self.metadata_df = metadata_df
         self.image_folder = image_folder
-        self.transform = transform
+        self.transform = transforms.Compose(
+                [transforms.Resize((224, 224)),
+                 transforms.ToTensor(), 
+                 transforms.Normalize(mean=[0.48828688, 0.42932517, 0.49162089], std=[0.41380908, 0.37492874, 0.41795654])]
+            )
 
     def __len__(self):
         return len(self.metadata_df)
@@ -20,10 +24,13 @@ class CustomCancerDataset(Dataset):
         image_name = os.path.join(self.image_folder, "{}_thumbnail.png".format(image_ids))
         # print(image_name)
         image = Image.open(image_name)
-        label = self.metadata_df.is_tma[idx]  
-        category = self.metadata_df.label[idx]
+        label_CC  = self.metadata_df.label_CC[idx]  
+        label_EC  = self.metadata_df.label_EC[idx]  
+        label_HGSC  = self.metadata_df.label_HGSC[idx]  
+        label_LGSC  = self.metadata_df.label_LGSC[idx]  
+        label_MC  = self.metadata_df.label_MC[idx]  
         
         if self.transform:
             image = self.transform(image)
 
-        return image, label, category
+        return image, [label_CC, label_EC, label_HGSC, label_LGSC, label_MC]
